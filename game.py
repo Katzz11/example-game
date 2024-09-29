aec6100cd68d69272b88aca06d29b34936686968
Katzz11 <katriin.poska@voco.ee>
2024-09-29 14:07:45 +0300

    ha

--- game.py
+++ game.py
@@ -1,11 +1,20 @@
 import pygame
+from settings import Settings
+
 pygame.init()
-#ekraan smh
-screen = pygame.display.set_mode([800, 500])
+gm_settings = Settings()
+
+screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
+pygame.display.set_caption(gm_settings.caption)
+
+
 running = True
 while running:
-        for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                        running = False
-        pygame.display.flip()
+    screen.fill(gm_settings.bg_color)    
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                    running = False
+
+    pygame.display.flip()
+
 pygame.quit() 
